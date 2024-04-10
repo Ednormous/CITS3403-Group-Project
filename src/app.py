ef7cb4559a92f5b29fa4866464315c982de6031d
Ednormous <edwardhaoyu319@gmail.com>
2024-04-10 17:03:07 +0800

    Fixed links to webpages
    Added more routes in app.py to various pages within the webapp
    
    web app to take routes with flask framework rather than relative path

--- src/app.py
+++ src/app.py
@@ -6,9 +6,28 @@ app.config['SECRET_KEY'] = 'we_love_writing_programs_123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 @app.route('/')
-
 def home():
     return render_template('homepage.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/index')
+def login():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/forgot-password')
+def forgot_password():
+    return render_template('forgot-password.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
