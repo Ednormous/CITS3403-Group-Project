a5d6e69e7f02dd7a4d79a647d9dc12e8d380484d
Ednormous <edwardhaoyu319@gmail.com>
2024-04-10 18:15:11 +0800

    Added database in the instance directory
    Created some necessary code to create the database.
    
    Created two tables
    1. User, 2. Role.
    The second table may not be necessary.
    
    CHANGES TO BE MADE: Split the app.py file into multiple smaller file for different purposes.
    
    styles.css file for css (TEST)

--- src/app.py
+++ src/app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'we_love_writing_programs_123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite' # Connects the database file to the app
+db = SQLAlchemy(app) # Creates the database instance
+
+# Creating table for database
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    role = db.Column(db.String(20), nullable=False)
+    
+class Role(db.Model): #A role table to store the roles of the users
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(20), nullable=False)
+
+# Create the database
+with app.app_context():
+    db.create_all()
+
+    existing_user = User.query.filter_by(username='admin').first()
+    if not existing_user:
+        admin = User(username='admin', password='admin', email='', role='admin')
+        db.session.add(admin)
+        db.session.commit()
+    else:
+        print('User already exists.')
 
 @app.route('/')
 def home():
