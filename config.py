9452b25df8b07fa0a95840fede830f7a0a4953df
Lucca-Sidey <115330681+Lucca-Sidey@users.noreply.github.com>
2024-05-18 20:20:06 +0800

    hardcoded units into the db

--- config.py
+++ config.py
@@ -2,16 +2,17 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'we_love_writing_programs_123'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
 
 class DeploymentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'instance/database.sqlite')
+
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     TESTING = True
-
